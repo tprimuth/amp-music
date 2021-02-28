dc6b73223471f1c5ef5634ca95205dd434ffd55d
Taylor Primuth <75693872+tprimuth@users.noreply.github.com>
2021-02-27 20:39:18 -0500

    updates

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -10266,7 +10266,15 @@ Promise.all([(0, _utils.preloadImages)('.gallery__item-imginner'), (0, _utils.pr
       return cursor.leave();
     });
   });
-});
+}); // const express = require('express');
+// const bodyParser = require('body-parser')
+// const mysql = require('mysqul')
+
+var app = express();
+var port = undefined || 5000; // app.use(bodyParser.urlencoded({ extended: false}))
+// app.use(bodyParser.json())
+// // MySQL codes
+// //listen on environment
 },{"./utils":"js/utils.js","./cursor":"js/cursor.js","./galleryController":"js/galleryController.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -10295,7 +10303,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50750" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54007" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
